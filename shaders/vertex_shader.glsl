0a9e7540f06fb4ca13a2693114dec75e42a2ffcc
Marius Debussche <marius.debussche@polytechnique.edu>
2021-09-17 15:39:16 +0200

    add glm

--- shaders/vertex_shader.glsl
+++ shaders/vertex_shader.glsl
@@ -2,7 +2,10 @@
 
 layout (location = 0) in vec3 position;
 
+uniform mat4 projection;
+
 void main()
 {
     gl_Position = vec4(position, 1.0);
 }
+    
--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #include <array>
 #include <initializer_list>
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // ************************************ //
 //          Global variables
 // ************************************ //
@@ -22,12 +26,20 @@ struct vec3 {
 	float z;
 
 	vec3(std::initializer_list<float> values) {
+		if (values.size() != 3) {
+			std::cout << "vec::invalid initializer size!\n";
+		}
 		auto it = values.begin();
 		x = *(it++);
 		y = *(it++);
 		z = *it;
 	}
 };
+
+std::ostream& operator<<(std::ostream& out, const vec3& vec) {
+	out << vec.x << '\t' << vec.y << '\t' << vec.z;
+	return out;
+}
 
 std::array<vec3, 8> cube_primitive_vertices = {
 	vec3({-0.5f, -0.5f, -0.5f}), vec3({0.5f, -0.5f, -0.5f}), vec3({0.5f, -0.5f, 0.5f}), vec3({-0.5f, -0.5f, 0.5f}),
@@ -49,8 +61,6 @@ std::array<int, 12 * 3> cube_primitive_indices = {
 	1, 5, 4
 };
 
-
-
 // ************************************ //
 //          Function headers
 // ************************************ //
@@ -61,11 +71,15 @@ void draw_data();             // Drawing calls within the animation loop
 /** Main function, call the general functions and setup the animation loop */
 int main()
 {
+	std::cout << sizeof(vec3) << '\n';
+	for (auto vec : cube_primitive_vertices) {
+		std::cout << vec << '\n';
+	}
 	std::cout << "*** Init GLFW ***" << std::endl;
 	glfw_init();
 
 	std::cout << "*** Create window ***" << std::endl;
-	auto window = glfw_create_window(500, 500, "My Window");
+	auto window = glfw_create_window(800, 600, "My Window");
 	glfwMakeContextCurrent(window);
 
 	std::cout << "*** Init GLAD ***" << std::endl;
@@ -82,7 +96,6 @@ int main()
 
 	std::cout << "*** Start GLFW loop ***" << std::endl;
 	while (!glfwWindowShouldClose(window)) {
-
 		draw_data();
 
 		glfwSwapBuffers(window);
@@ -97,28 +110,33 @@ int main()
 /** Create (or load) data and send them to GPU */
 void load_data()
 {
-	// *** Send data on the GPU
 	GLuint vbo = 0;
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, cube_primitive_vertices.size() * sizeof(GLfloat) * 3, cube_primitive_vertices.data(), GL_STATIC_DRAW);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	// *** Set shader attributes
-	glGenVertexArrays(1, &vao);
-	glBindVertexArray(vao);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
-
 	GLuint ebo = 0;
+	
+	glGenBuffers(1, &vbo);
+	glGenVertexArrays(1, &vao);
 	glGenBuffers(1, &ebo);
+
+	glBindVertexArray(vao);
+
+	// *** Send data on the GPU
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, cube_primitive_vertices.size() * sizeof(vec3), cube_primitive_vertices.data(), GL_STATIC_DRAW);
+
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, cube_primitive_indices.size() * sizeof(int), cube_primitive_indices.data(), GL_STATIC_DRAW);
 
+	// *** Set shader attributes	
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), nullptr);
+
+	
 	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	// glEnable(GL_CULL_FACE);
+	// glCullFace(GL_FRONT);  
 }
 
 
@@ -129,21 +147,24 @@ void draw_data()
 	// ******************************** //
 	// Clear screen
 	// ******************************** //
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT);
+
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 
 	// ******************************** //
 	// Draw data
 	// ******************************** //
 	glUseProgram(shader_program);
-	glBindVertexArray(vao);
 
+	glUniformMatrix4fv(glGetUniformLocation(shader_program, "projection"), 1, GL_FALSE, &projection[0][0]);
 	// Draw call
-	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, 12*3, GL_UNSIGNED_INT, 0);
 
-	//glBindVertexArray(0);
-	//glUseProgram(0);
+	glBindVertexArray(0);
+	glUseProgram(0);
 }
 
 
